dce58690a5f2912848e1dfc83891f8f2841bbb58
Philip Johnson <johnson@hawaii.edu>
2020-12-04 06:57:25 -1000

    Add dropdown initiatives to navbar

--- ui/src/NavBar.tsx
+++ ui/src/NavBar.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import {Menu, Image, Button, Popup} from 'semantic-ui-react';
+import {Menu, Image, Button, Popup, Modal, Dropdown} from 'semantic-ui-react';
 import uhLogo from './images/uh-logo.png';
 import {NavLink} from 'react-router-dom';
 
-const PopupExample = () => (
-  <Popup content='If this button actually worked, then clicking it would download all the data in this dashboard for personal exploration.' trigger={<Button basic>Export Data</Button>} />
-)
+function ExportModalButton() {
+  const [open, setOpen] = React.useState(false)
+  return (
+    <Modal onClose={() => setOpen(false)} onOpen={() => setOpen(true)} open={open}
+           trigger={<Button basic>Export</Button>}>
+      <Modal.Header>Export Data</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>If this button actually worked, then clicking it would download all the data in this
+          dashboard for personal exploration.</Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button content="Dismiss" onClick={() => setOpen(false)} positive/>
+      </Modal.Actions>
+    </Modal>
+  )
+}
 
-type NavBarProps =  {
+type NavBarProps = {
   title: string
 }
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
-const NavBar: React.FunctionComponent<NavBarProps> = ({ title = 'UH Dept Dashboard' }) => {
+const NavBar: React.FunctionComponent<NavBarProps> = ({title = 'UH Dept Dashboard'}) => {
   const menuStyle = {marginBottom: '20px'};
   return (
     <Menu style={menuStyle} attached="top" borderless>
@@ -23,8 +36,14 @@ const NavBar: React.FunctionComponent<NavBarProps> = ({ title = 'UH Dept Dashboa
         <span className='uh-green' style={{fontWeight: 800, fontSize: '24px'}}>{title}</span>
       </Menu.Item>
       <Menu.Item as={NavLink} activeClassName="active" exact to="/" key='academic-year'>Academic Year View</Menu.Item>
-      <Menu.Item as={NavLink} activeClassName="active" exact to="/timeline" key='timeline'>Timeline View</Menu.Item>
-      <Menu.Item><PopupExample/></Menu.Item>
+      <Menu.Item>
+        <Dropdown text="Initiatives View">
+          <Dropdown.Menu>
+            <Dropdown.Item text="Diversity" as={NavLink} exact to="/timeline"/>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Item>
+      <Menu.Item><ExportModalButton/></Menu.Item>
     </Menu>
   );
 }
